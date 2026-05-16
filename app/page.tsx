5252fc2f263215c4429c49af330305e424babe5c
Talha Salman <talhahsalman-boop>
2026-05-16 15:58:43 +0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -18,20 +18,7 @@ export default function Home() {
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Hn Apna hi page hai
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+           
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
